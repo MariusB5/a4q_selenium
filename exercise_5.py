76a066db4308f51c26aac79ff72e3c1dc7bcf8f4
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-13 18:41:02 -0400

    started ex 5

---
+++ exercise_5.py
@@ -0,0 +1,4 @@
+# Same as exercise 4 but now the password field will be located using friendly locator.
+# First the textbox for the username needs to be found using the id locator then the browser will find the
+# input tag element just below the username textbox to enter the password.
+
