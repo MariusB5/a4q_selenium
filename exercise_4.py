493ae09580356ea9bc76f5a271dc7b7d5e20ffca
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-13 17:58:23 -0400

    started exercise 4

---
+++ exercise_4.py
@@ -0,0 +1,3 @@
+# Same as exercise 4 with the implementation of a try catch construct. The automated test needs to be
+# wrapped in a function named ‘login_to_saucedemo’ which will be called to execute the test.
+
