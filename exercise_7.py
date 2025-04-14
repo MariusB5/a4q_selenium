90e1bbfa54cb4a8ea833ea16b4d1f0ab1bbad9c3
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-14 13:00:57 -0400

    started ex 7

---
+++ exercise_7.py
@@ -0,0 +1,6 @@
+# Same as exercise 6 but now as the user is successfully logged in, a partial screen capture of the third item
+# on the inventory list (with its product description and the add to card button) will be taken and saved to the
+# main project directory under the filename inventory_3.png.
+# Example of the main project directory is C:\Users\<Your UserName>\PycharmProjects\<Your project
+# name>\<your subfolder>
+
