4d78a57238e11462ce943b4c051fb0fa76a57c9a
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-14 12:59:36 -0400

    ex 6 finished

--- exercise_6.py
+++ exercise_6.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import requests
+import time
 
 
 PATH = Service("C:\\Users\\marius\\chromedriver.exe")
@@ -23,23 +23,24 @@ password = "secret_sauce"
 
 def login_to_saucedemo():
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            driver.get(url)
-            WebDriverWait(driver, 180).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#root > div > div.login_logo')))
-            user_field = driver.find_element(By.ID, 'user-name')
-            user_field.send_keys(user_name)
-            password_field = driver.find_element(locate_with(By.TAG_NAME, 'input').below({By.ID:'user-name'}))  # overcomplicated way to locate an element
-            password_field.send_keys(password)
-            driver.find_element(By.ID, 'login-button').click()
-            wait = WebDriverWait(driver, 5)
-            inventory_container = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'inventory_container')))
-            if inventory_container.get_attribute("id") == "inventory_container":
-                    print("Login successful!")
-            else:
-                    print("Login failed!")
+        driver.get(url)
+        
+        time_limit = 180
+        while driver.execute_script("return document.readyState") != "complete" and time_limit < 180:
+            time.sleep(1)
+            time_limit += 1
+        
+        user_field = driver.find_element(By.ID, 'user-name')
+        user_field.send_keys(user_name)
+        password_field = driver.find_element(locate_with(By.TAG_NAME, 'input').below({By.ID:'user-name'}))  # overcomplicated way to locate an element
+        password_field.send_keys(password)
+        driver.find_element(By.ID, 'login-button').click()
+        wait = WebDriverWait(driver, 10)
+        inventory_container = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'inventory_container')))
+        if inventory_container.get_attribute("id") == "inventory_container":
+                print("Login successful!")
         else:
-            print("Web page not loaded.")
+                print("Login failed!")
     
     except Exception as e:
         print(f"An error occurred: {type(e).__name__}")
