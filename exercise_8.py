08ac6fa60ea90891c94799f0aa3eebb8a2e50128
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-14 18:48:21 -0400

    started ex 8

---
+++ exercise_8.py
@@ -0,0 +1,8 @@
+# Same as exercise 7 but now the browser will be launched in headless mode. After login, a new tab will be
+# opened to navigate to ‘https://www.saucedemo.com/cart.html’. After navigation, the context will switch back
+# to the original tab and a partial screen capture of the third item on the inventory list (with its product
+# description and the add to card button) will be taken and saved as inventory_three.png in the main project
+# directory.
+# Example of the main project directory is C:\Users\<Your UserName>\PycharmProjects\<Your project
+# name>\<your subfolder>
+
